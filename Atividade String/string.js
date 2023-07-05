4235dfd46ea6b1f0ace4acbead426de8f1e5fe6f
Camila <125229387+camilarib@users.noreply.github.com>
2023-07-05 16:45:29 -0300

    Add files via upload

---
+++ Atividade String/string.js
@@ -0,0 +1,34 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector("pre");
+
+frm.addEventListener("submit", (e) => {
+    //evita o envio do formulário
+    e.preventDefault();
+
+    //obter os valores digitados
+    //const nome = frm.inNome.value;
+    const Frase = String(frm.inFrase.value);
+    const palavra = String(frm.inpalavra.value);
+    const posicao = String(frm.inposicao.value);
+
+    console.log(insertString(Frase, palavra, posicao));
+})
+
+function insertString(Frase, palavra, posicao){
+    const charArray = Frase.split(""); 
+        insertCount = 0; 
+
+    let indexes = Mudaposicao.split(',');
+
+    indexes = indexes.map(index => parseInt (index.trim()));//converte o index para inteiro
+
+        for(let i = 0; i <= str.length; i++){
+            //console.log(str[i]); 
+            if(indexes.includes(i)){
+                charArray.splice(i + insertCount, 0, palavra); 
+                insertCount++
+            }
+        }
+        return charArray.join(""); 
+}
+//console.log(insertString("O Julio esta maluco", "de", [0,8]))
